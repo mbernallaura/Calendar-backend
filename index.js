a62053c86a8360f74882973c2042f0ead8d7ec51
laura <mbernallaura@gmail.com>
2024-10-30 18:37:40 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//! Haciendo Backend server
+const express = require('express');
+
+//Crear el servidor de express
+const app = express();
+
+//Rutas
+app.get('/', (req, res)=>{
+    res.json({
+        ok: true
+    })
+});
+
+//Escuchar peticiones
+app.listen(3000, () =>{
+    console.log(`Servidor corriendo en puerto ${ 3000 }`);
+    
+});
