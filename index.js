56247c54caf108078b308d7aff88d99f64fecd83
laura <mbernallaura@gmail.com>
2024-11-06 18:58:31 -0500

    Configurar variables de entorno, poner a funcionar el index.html

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 //! Haciendo Backend server
 const express = require('express');
+require('dotenv').config();
+
+console.log(process.env);
+
 
 //Crear el servidor de express
 const app = express();
 
+//Directorio Publico
+app.use( express.static('public') ); //!.use() = es un midelware que es una funcion que se ejecuta siempre que pasa en algun lugar 
+
 //Rutas
-app.get('/', (req, res)=>{
-    res.json({
-        ok: true
-    })
-});
+// app.get('/', (req, res)=>{
+//     res.json({
+//         ok: true
+//     })
+// });
 
 //Escuchar peticiones
-app.listen(3000, () =>{
-    console.log(`Servidor corriendo en puerto ${ 3000 }`);
+app.listen(process.env.PORT, () =>{
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
     
 });
