a21dcba8ef48cfdfb46cc3eeadf1d7f56ecee2b8
Mayorga-Bernal Laura <laura.mayorga@inetum.com>
2025-10-08 09:40:47 -0500

    Configurar express, crear variable de entorno, hacer un request

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 //! Haciendo Backend server
+//?Configurando express
 const express = require('express');
+
+//?Configurar .env
 require('dotenv').config();
+//console.log(process.env);
 
-console.log(process.env);
 
-
-//Crear el servidor de express
+//? Crear el servidor de express
 const app = express();
 
-//Directorio Publico
-app.use( express.static('public') ); //!.use() = es un midelware que es una funcion que se ejecuta siempre que pasa en algun lugar 
+//?Directorio Publico
+//! .use() = es un midelware que es una funcion que se ejecuta siempre que pasa en algun lugar, cuando alguien haga una peticion
+app.use( express.static('public') ); 
 
-//Rutas
+//?Rutas
+//! Todo el archivo ./routes/auth que esta en require va a ser exportado y lo habilitara en la ruta /api/auth
+app.use('/api/auth', require('./routes/auth'));
+//TODO: auth, crear, login, renew, crud de ventos
 // app.get('/', (req, res)=>{
 //     res.json({
 //         ok: true
 //     })
 // });
 
-//Escuchar peticiones
+//?Escuchar peticiones
+// app.listen(4000, ()=>{
+//     console.log("Servidor corriendo");
+// })
 app.listen(process.env.PORT, () =>{
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
     
