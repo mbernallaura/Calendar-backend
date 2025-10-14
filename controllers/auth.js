4b4336e4ff1ebaedf1a19e6e740d6ad74364407a
Mayorga-Bernal Laura <laura.mayorga@inetum.com>
2025-10-14 17:25:10 -0500

    crear carpeta parra los custom middleware y para que quede sectorizado las validaciones

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,15 +6,11 @@ se hace esto es para obtener la ayuda(intelliSense) al hacer el request y el res
 //const express = require('express');
 //Se desestructura lo anterior para hacer mas corto el codigo
 const { response } = require("express");
-const { validationResult } = require('express-validator');
-
 
 //!Request(req)  = Lo que la persona solicita
 //!Response(res) = Lo que nosotros respondemos
 const crearUsuario = (req, res = response)=>{
     const { name,  email, password } = req.body;
-
-    //? manejo de errores
 
     //Sin express-validator
     // if (name.length < 5) {
@@ -25,16 +21,15 @@ const crearUsuario = (req, res = response)=>{
     // }
 
     //Express-validator
-    const errors = validationResult( req );
-    if ( !errors.isEmpty() ) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
-    console.log(errors);
+    // const errors = validationResult( req );
+    // if ( !errors.isEmpty() ) {
+    //     return res.status(400).json({
+    //         ok: false,
+    //         errors: errors.mapped()
+    //     });
+    // }
+    // console.log(errors);
     
-
     res.status(201).json({
         ok: true,
         msg: 'registro',
@@ -46,15 +41,6 @@ const crearUsuario = (req, res = response)=>{
 
 const loginUsuario = (req, res = response)=>{
     const { email, password } = req.body;
-
-    //express-validator
-    const errors = validationResult( req );
-    if ( !errors.isEmpty() ) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
 
     res.status(201).json({
         ok: true,
