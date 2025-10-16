f89f0f2b38c56b07a146b7741b090f8363e57ab8
mbernallaura <mbernallaura@gmail.com>
2025-10-16 17:31:37 -0500

    validar el correo sin dejar que haga el mongoose todo el trabajo, crear el modelo para un usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,11 +6,41 @@ se hace esto es para obtener la ayuda(intelliSense) al hacer el request y el res
 //const express = require('express');
 //Se desestructura lo anterior para hacer mas corto el codigo
 const { response } = require("express");
+const Usuario = require('../models/Usuario');
 
 //!Request(req)  = Lo que la persona solicita
 //!Response(res) = Lo que nosotros respondemos
-const crearUsuario = (req, res = response)=>{
+const crearUsuario = async (req, res = response)=>{
     const { name,  email, password } = req.body;
+
+    try {
+        // manda el usuario existente
+        let usuario = await Usuario.findOne({ email });
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: "Un usuario existe con ese correo"
+            })
+        }
+        
+        usuario = new Usuario(req.body);
+
+        //? Guardar en base de datos
+        //! .save() es una promesa y va guardar los datos o mandara un error
+        await usuario.save();
+
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name
+        })
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: "Por favor hable con el administrador"
+        })
+    }
+
 
     //Sin express-validator
     // if (name.length < 5) {
@@ -30,13 +60,6 @@ const crearUsuario = (req, res = response)=>{
     // }
     // console.log(errors);
     
-    res.status(201).json({
-        ok: true,
-        msg: 'registro',
-        name,
-        email,
-        password
-    })
 }
 
 const loginUsuario = (req, res = response)=>{
