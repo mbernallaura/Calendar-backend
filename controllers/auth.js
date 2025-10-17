f1a5a91ddf4f4ddd640ef84054a4969d444215d3
Mayorga-Bernal Laura <laura.mayorga@inetum.com>
2025-10-17 16:48:54 -0500

    encriptar la contraseña, instalar bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,14 +2,16 @@
 solo recarga la que ya se habia hecho 
 se hace esto es para obtener la ayuda(intelliSense) al hacer el request y el response en las peticiones
 */
+//!Request(req)  = Lo que la persona solicita
+//!Response(res) = Lo que nosotros respondemos
 
 //const express = require('express');
 //Se desestructura lo anterior para hacer mas corto el codigo
 const { response } = require("express");
+const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
 
-//!Request(req)  = Lo que la persona solicita
-//!Response(res) = Lo que nosotros respondemos
+
 const crearUsuario = async (req, res = response)=>{
     const { name,  email, password } = req.body;
 
@@ -22,8 +24,15 @@ const crearUsuario = async (req, res = response)=>{
                 msg: "Un usuario existe con ese correo"
             })
         }
-        
+        //recolecta info que se manda del body
         usuario = new Usuario(req.body);
+
+        //? Encriptar contraseña
+        //! salt = se genera numeros o letras al azar para poder encripar la contraseña
+        //! .genSaltSync() = por defecto trae de largo 10, entre mas grande sea el numero 
+        //! mas compleja y segura sera la contraseña pero va a tardar mas en hacer el hash
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync( password, salt );
 
         //? Guardar en base de datos
         //! .save() es una promesa y va guardar los datos o mandara un error
