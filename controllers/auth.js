c037022f808ebe1aa554d7ffebb2cb113cae3258
mbernallaura <mbernallaura@gmail.com>
2025-10-23 16:35:43 -0500

    revalidar el token y saber si el token que se manda sea ell mismo, instalar el cors emplearlo

--- controllers/auth.js
+++ controllers/auth.js
@@ -118,10 +118,15 @@ const loginUsuario = async (req, res = response)=>{
         });
     }
 }
-const revalidarToken = (req, res = response)=>{
+const revalidarToken = async (req, res = response)=>{
+    const { uid, name } = req;
+
+    // Generar un nuevo token
+    const token = await generarJWT( uid, name );
+
     res.json({
         ok: true,
-        msg: 'renew'
+        token
     })
 }
 
