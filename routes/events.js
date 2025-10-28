0a2b9555f6b75c2214b95ddf53e6952609ec8dc2
laura <mbernallaura@gmail.com>
2025-10-28 16:31:30 -0500

    realizar el esquemas para evento y la ruta para el mismo

--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,14 @@
 const { Router } = require('express');
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 // Rutas
 // host + /api/events
+
+//! Cualquier peticion que se encuentre debajo va tener que usar lo que se indica en este caso
+//! todas las peticiones van a pasar por el validarjwt
+router.use(validarJWT);
 
 //Obtener evento
 // host + /api/events/events
